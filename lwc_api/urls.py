6aa88d2b7a4b4fbf2428453afbec65507d7b7f8e
Mauricio <36507898+darketmaster@users.noreply.github.com>
2021-07-04 23:32:31 -0500

    update Admin title

--- lwc_api/urls.py
+++ lwc_api/urls.py
@@ -1,6 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 from . import views
+from django.contrib import admin
+
+admin.site.site_header = "Ambiental Quality LWC IoT Admin"
+admin.site.site_title = "Ambiental Quality LWC IoT  Admin Portal"
+admin.site.index_title = "Welcome to Ambiental Quality LWC IoT Portal"
 
 router = routers.DefaultRouter()
 router.register(r'devices', views.DeviceViewSet)
